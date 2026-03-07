124bee225850479e7e5815a007c535e54bfebe3d
lectiopedia <docoutofabricio@gmail.com>
2026-03-07 15:06:35 -0300

    Create catalogo.js

---
+++ catalogo.js
@@ -0,0 +1,21 @@
+const catalogo = {
+
+"Ventas y Marketing":[
+"Vendes o Vendes",
+"El arte de cerrar la venta",
+"Ventas 101",
+"Las 22 leyes inmutables del marketing",
+"El vendedor millonario"
+],
+
+"Psicologia":[
+"Psicologia del dinero",
+"Pensar rapido pensar despacio"
+],
+
+"Negocios":[
+"Padre rico padre pobre",
+"La semana laboral de 4 horas"
+]
+
+};
